979d87a51723f4c1881412dfda33139d207f4b65
fabrice137 <fabruz3@gmail.com>
2024-09-27 20:26:54 +0200

    modified the js code of bac tracker

--- bac/app.js
+++ bac/app.js
@@ -1,104 +1,115 @@
 
-     
-      function getTimeValue() {
-        let textHours = document.getElementById("time-display-hour").textContent;
-        let textMinutes = document.getElementById("time-display-minute").textContent;
+let drinksArray = [];
 
-        let hours = parseInt(textHours.substring(0, textHours.length -1));
-        let minutes = parseInt(textMinutes.substring(0, textMinutes.length -1));
+function getTimeValue() {
+  let textHours = document.getElementById("time-display-hour").textContent;
+  let textMinutes = document.getElementById("time-display-minute").textContent;
 
-        return minutes + (hours * 60);
-      }
+  let hours = parseInt(textHours.substring(0, textHours.length -1));
+  let minutes = parseInt(textMinutes.substring(0, textMinutes.length -1));
 
-      function setTimeValue(minutes) {
-        if(minutes <= 0) minutes = 0;
-        let minText = (minutes % 60 === 0) ? "00" : "30";
-        document.getElementById("time-display-hour").textContent = Math.floor(minutes / 60) +"h";
-        document.getElementById("time-display-minute").textContent = minText +"\"";
-      }
+  return minutes + (hours * 60);
+}
 
-      function getHours(minutes){
-        return Math.floor(minutes / 60);
-      }
-      function getMinutesLeft(minutes){
-        return Math.floor(minutes % 60);
-      }
+function increaseWeightValue() {
+  let weight = parseInt(document.getElementById("weight-value").textContent);
+  if(weight >= 300) return weight;
 
-      function increamentTimeValue(){
-        let time = getTimeValue();
-        time += 30;
-        
-        setTimeValue(time);
-      }
+  document.getElementById("weight-value").textContent = (weight + 5);
+}
 
-      function decreamentTimeValue(){
-        let time = getTimeValue();
-        time -= 30;
-        setTimeValue(time);
-      }
+function decreaseWeightValue() {
+  let weight = parseInt(document.getElementById("weight-value").textContent);
+  if(weight <= 10) return weight;
 
-      function calculateBAC() {
+  document.getElementById("weight-value").textContent = (weight - 5);
+}
 
-        let time = getTimeValue();
+function setTimeValue(minutes) {
+  if(minutes <= 0) minutes = 0;
+  let minText = (minutes % 60 === 0) ? "00" : "30";
+  document.getElementById("time-display-hour").textContent = Math.floor(minutes / 60) +"h";
+  document.getElementById("time-display-minute").textContent = minText +"\"";
+}
 
-        let beer = getAlcoholGrams('beer');
-        let wine = getAlcoholGrams('wine');
-        let liquor = getAlcoholGrams('liquor');
-        let other = getOthersAlcoholGram();
+function getHours(minutes){
+  return Math.floor(minutes / 60);
+}
+function getMinutesLeft(minutes){
+  return Math.floor(minutes % 60);
+}
 
-        // BAC = (alcoholGrams / bodyFactor) * 100;
-        let bac = ((beer + wine + liquor + other) / getBodyFactor()) * 100;
-        
-        let leftBAC = bac - ((getHours(time) * 0.015) + ((0.015 / 2) * (getMinutesLeft(time) ? 1 : 0)));
-        let textBAC = (leftBAC +"").substring(0, 5);
-        if(leftBAC < 0.0005) textBAC = "0.00";
+function increaseTimeValue(){
+  let time = getTimeValue();
+  time += 30;
+  
+  setTimeValue(time);
+}
 
-        document.getElementById('final-result').textContent = "Your BAC is: "+ textBAC +"%";
-        // console.log("BAC: "+ bac);
-        // console.log("Left BAC: "+ leftBAC);
-        // console.log("=================================");
+function decreaseTimeValue(){
+  let time = getTimeValue();
+  time -= 30;
+  setTimeValue(time);
+}
 
-      }
+function addDrink(){
+  let txtAmount = document.getElementById("drink-amount").value;
+  let txtSize = document.getElementById("drink-size").value;
+  let txtAbv = document.getElementById("drink-abv").value;
+
+  let amount = (txtAmount && !isNaN(txtAmount)) ? parseInt(txtAmount) : 0;
+  let size = (txtSize && !isNaN(txtSize)) ? parseInt(txtSize) : 0;
+  let abv = (txtAbv && !isNaN(txtAbv)) ? parseFloat(txtAbv) : 0;
+
+  let txt = `{hm: ${amount}, gsz: ${size}, abv: ${abv}}`;
+  let pseudo_id = amount +"-"+ size +"-"+ abv;
+
+  drinksArray.push({id: pseudo_id, amount: amount, size: size, abv: abv});
+
+  let addHere = document.getElementById("drinks-added-here");
+  addHere.innerHTML += `<div class="drink-item fx-row fx-cntr" id="cl-${pseudo_id}"><span> ${txt} </span>
+      <button id="${pseudo_id}" class="btn-remove" onclick="removeDrink(this.id)">x</button></div>`;
+}
+
+function removeDrink(clickedId){
+  document.getElementById("cl-"+clickedId).remove();
+  for (let i = 0; i < drinksArray.length; i++) {
+    if(drinksArray[i].id === clickedId) drinksArray.splice(i, 1);    
+  }
+
+}
+
+function calculateBAC(){
+  let totalAlcoholInGrams = 0;
+
+  for(let i = 0; i < drinksArray.length; i++){
+    let drinksGram = getAlcoholGrams(drinksArray[i].amount, drinksArray[i].size, drinksArray[i].abv);
+    totalAlcoholInGrams += drinksGram;
+  }
+
+  let time = getTimeValue();
+  let metabolizedBAC = ((getHours(time) * 0.015) + ((0.015 / 2) * (getMinutesLeft(time) ? 1 : 0)));
+  
+  let bacMale = ((totalAlcoholInGrams / getBodyFactor('male')) * 100) - metabolizedBAC;
+  let bacFemale = ((totalAlcoholInGrams / getBodyFactor('female')) * 100) - metabolizedBAC;
+
+  console.log("bac male: "+ bacMale)
+  console.log("bac female: "+ bacFemale)
 
 
-      function getAlcoholGrams(drinkType){
-        let amountText = document.getElementById(drinkType +'-amount').value;
-        let amount = (amountText && !isNaN(amountText)) ? parseInt(amountText) : 0;
+  document.getElementById('male-final-result').textContent = "Your BAC is: "+ bacMale.toFixed(3) +"% Man!";
+  document.getElementById('female-final-result').textContent = "Your BAC is: "+ bacFemale.toFixed(3) +"% Girl!";
 
-        let size = getSelectedValue(drinkType +'-size');
+}
 
-        let abv = getSelectedValue(drinkType +'-abv');
-        
-        return size * (abv / 100) * 0.789 * amount; 
-      }
+function getAlcoholGrams(amount, size, abv){  
+  return size * (abv / 100) * 0.789 * amount; 
+}
 
-      function getOthersAlcoholGram(){
-        let amountText = document.getElementById('other-amount').value;
-        let sizeText = document.getElementById('other-size').value;
-        let abvText = document.getElementById('other-abv').value;
 
-        let amount = (amountText && !isNaN(amountText)) ? parseInt(amountText) : 0;
-        let size = (sizeText && !isNaN(sizeText)) ? parseInt(sizeText) : 0;
-        let abv = (abvText && !isNaN(abvText)) ? parseInt(abvText) : 0;
-        
-        return size * (abv / 100) * 0.789 * amount; 
-      }
+function getBodyFactor(gender){
+  let weight = parseInt(document.getElementById('weight-value').innerText);
+  return weight * 1000 * ((gender === 'male') ? 0.68 : 0.55);
+}
 
-      function getSelectedValue(id) {
-        let dropDown = document.getElementById(id);
-        let selectedOption = dropDown.options[dropDown.selectedIndex];
-        return selectedOption.value;
-      }
 
-      function getBodyFactor(){
-        let genderElement = document.getElementsByName('gender');
-        let weight = parseInt(document.getElementById('body-weight').value);
-
-        let gender = "male";
-        for (let i = 0; i < genderElement.length; i++) 
-          if (genderElement[i].checked) gender = genderElement[i].value;
-
-        return weight * 1000 * ((gender === 'male') ? 0.68 : 0.55);
-      }
-
-      
